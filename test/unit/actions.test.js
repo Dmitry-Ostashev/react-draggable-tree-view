0b2e91baa2b69ff630a372026bc88aeae0f9f9e4
Dmitry-Ostashev <dmitry.ostashev@devexpress.com>
2021-10-12 01:06:36 +0800

    UT for actions

---
+++ test/unit/actions.test.js
@@ -0,0 +1,16 @@
+const { ACTION_TYPES, reducer } = require('../../src/tree-view-component/actions');
+const { listData }              = require('../data/test-data');
+
+describe(ACTION_TYPES.START_NODE_DRAGGING, () => {
+    it('should set draggingNodeId', () => {});
+});
+
+describe(ACTION_TYPES.UPDATE_NODE_DROP_PLACE, () => {
+    it('should calculate new drop place', () => {});
+});
+
+describe(ACTION_TYPES.STOP_NODE_DRAGGING, () => {
+    it('should drop into a right place', () => {
+
+    });
+});
--- test/unit/data-converters.test.js
+++ test/unit/data-converters.test.js
@@ -1,48 +1,5 @@
 const { treeToList, listToTree, updateNodeParent } = require('../../src/utils/data-converter');
-
-const treeData = [
-    { rowId: 1, name: 'salads' },
-    {
-        rowId: 2, name: 'drinks', children: [
-            { rowId: 3, name: 'juice' },
-            { rowId: 4, name: 'water' },
-            {
-                rowId: 5, name: 'alcohol', children: [
-                    { rowId: 6, name: 'whiskey' },
-                    { rowId: 7, name: 'wine' }
-                ]
-            },
-            {
-                rowId: 8, name: 'coctails', children: [
-                    { rowId: 9, name: 'whiskey & soda' },
-                    { rowId: 10, name: 'white russian' }
-                ]
-            }
-        ]
-    },
-    {
-        rowId: 11, name: 'cookies', children: [
-            { rowId: 12, name: 'chocolate cookies' },
-            { rowId: 13, name: 'bakery cookies' }
-        ]
-    }
-];
-
-const listData = [
-    { node: { rowId: 1, name: 'salads' }, depth: 1, parentId: -1 },
-    { node: { rowId: 2, name: 'drinks'}, depth: 1, parentId: -1 },
-    { node: { rowId: 3, name: 'juice' }, depth: 2, parentId: 2 },
-    { node: { rowId: 4, name: 'water' }, depth: 2, parentId: 2 },
-    { node: { rowId: 5, name: 'alcohol'}, depth: 2, parentId: 2 },
-    { node: { rowId: 6, name: 'whiskey' }, depth: 3, parentId: 5 },
-    { node: { rowId: 7, name: 'wine' }, depth: 3, parentId: 5 },
-    { node: { rowId: 8, name: 'coctails'}, depth: 2, parentId: 2 },
-    { node: { rowId: 9, name: 'whiskey & soda'}, depth: 3, parentId: 8 },
-    { node: { rowId: 10, name: 'white russian'}, depth: 3, parentId: 8 },
-    { node: { rowId: 11, name: 'cookies' }, depth: 1, parentId: -1 },
-    { node: { rowId: 12, name: 'chocolate cookies' }, depth: 2, parentId: 11 },
-    { node: { rowId: 13, name: 'bakery cookies' }, depth: 2, parentId: 11 },
-];
+const { treeData, listData }                       = require('../data/test-data');
 
 describe('treeToList()', () => {
     it('should convert', () => {
